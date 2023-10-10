3a31a27dd0ff9524116e748948854cfe93b3a942
Julian Poyourow <julian@poyourow.com>
2023-10-09 22:42:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Essentials
-sudo apt-get install -y curl gcc
+sudo apt-get install -y curl gcc jq htop tmux vim
 
 # Fish
-sudo apt-add-repository ppa:fish-shell/release-3 -y
-sudo apt-get update
-sudo apt-get install fish -y
-sudo apt-get install fonts-powerline -y
+sudo apt-get install fish fonts-powerline -y
 sudo chsh -s /usr/bin/fish $USER
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 fish -c "fisher install oh-my-fish/theme-bobthefish"
@@ -18,7 +15,7 @@ fish -c "fish /tmp/fish-aliases.sh"
 fish -c "fisher install jorgebucaran/nvm.fish"
 fish -c "nvm install 18"
 fish -c "set --universal nvm_default_version 18"
-fish -c "set --universal nvm_default_packages yarn neovim"
+fish -c "set --universal nvm_default_packages yarn nx neovim"
 
 # NeoVim
 mkdir ~/.neovim-bin
@@ -32,18 +29,12 @@ mkdir -p $HOME/.config/nvim
 curl -sL $DOTFILES_URI/raw/master/init.lua -o $HOME/.config/nvim/init.lua
 
 sudo apt-get install python3 python3-pip -y
-sudo apt-get install python3 -y
 pip3 install pynvim
 
-sudo apt-get install ripgrep -y
-sudo apt-get install fd-find -y
+sudo apt-get install ripgrep fd-find -y
 
-# Tmux
-sudo apt-get install tmux -y
+# Tmux config
 curl -sL $DOTFILES_URI/raw/master/.tmux.conf -o $HOME/.tmux.conf
-
-# Other
-sudo apt-get install -y htop
 
 # Ngrok
 curl -sL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -o /tmp/ngrok-v3-stable-linux-amd64.tgz
