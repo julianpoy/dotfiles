369e37b29d517962f7faa382eec1dfb5e1a60781
Julian Poyourow <julian@poyourow.com>
2023-10-12 23:45:48 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Essentials
-sudo apt-get install -y curl gcc jq htop tmux vim
+sudo apt-get install -y \
+  curl gcc jq zip unzip htop tmux vim python3 python3-pip \
+  python3-pynvim \
+  fish fonts-powerline \
+  ripgrep fd-find
+
 sudo apt-get install -y netcat || sudo apt-get install -y netcat-openbsd
 
 # Fish
-sudo apt-get install fish fonts-powerline -y
 sudo chsh -s /usr/bin/fish $USER
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 fish -c "fisher install oh-my-fish/theme-bobthefish"
@@ -14,9 +17,9 @@ fish -c "fish /tmp/fish-aliases.sh"
 
 # NVM
 fish -c "fisher install jorgebucaran/nvm.fish"
-fish -c "nvm install 18"
 fish -c "set --universal nvm_default_version 18"
 fish -c "set --universal nvm_default_packages yarn nx neovim typescript"
+fish -c "nvm install 18"
 
 # NeoVim
 mkdir ~/.neovim-bin
@@ -28,11 +31,6 @@ fish -c "alias vim='~/.neovim-bin/squashfs-root/usr/bin/nvim' && funcsave vim"
 
 mkdir -p $HOME/.config/nvim
 curl -sL $DOTFILES_URI/raw/master/init.lua -o $HOME/.config/nvim/init.lua
-
-sudo apt-get install python3 python3-pip -y
-pip3 install pynvim
-
-sudo apt-get install ripgrep fd-find -y
 
 # Tmux config
 curl -sL $DOTFILES_URI/raw/master/.tmux.conf -o $HOME/.tmux.conf
