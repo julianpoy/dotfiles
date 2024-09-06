b5defc29b680989fd004e4a519a075c0f14dc5bb
Julian Poyourow <julian@poyourow.com>
2024-09-06 09:17:14 -0700

    Update neovim to 0.10.1, remove ngrok, remove tree-sitter-cli

--- install.sh
+++ install.sh
@@ -23,16 +23,16 @@ fish -c "fish /tmp/fish-aliases.sh"
 # NVM
 fish -c "fisher install jorgebucaran/nvm.fish"
 fish -c "set --universal nvm_default_version 20"
-fish -c "set --universal nvm_default_packages yarn nx neovim typescript tree-sitter-cli"
+fish -c "set --universal nvm_default_packages yarn nx neovim typescript"
 fish -c "nvm install 20"
 
 # Nx
 fish -c "set -Ux NX_REJECT_UNKNOWN_LOCAL_CACHE 0"
 
 # NeoVim
-rm -rf ~/.neovim-bin
+sudo rm -rf ~/.neovim-bin
 mkdir ~/.neovim-bin
-wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz -O ~/.neovim-bin/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz -O ~/.neovim-bin/nvim-linux64.tar.gz
 sudo tar -C ~/.neovim-bin -xzf ~/.neovim-bin/nvim-linux64.tar.gz
 fish -c "alias vim='~/.neovim-bin/nvim-linux64/bin/nvim' && funcsave vim"
 
@@ -41,10 +41,6 @@ curl -sL $DOTFILES_URI/raw/master/init.lua -o $HOME/.config/nvim/init.lua
 
 # Tmux config
 curl -sL $DOTFILES_URI/raw/master/.tmux.conf -o $HOME/.tmux.conf
-
-# Ngrok
-curl -sL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -o /tmp/ngrok-v3-stable-linux-amd64.tgz
-sudo tar xvzf /tmp/ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 
 # Git config
 git config --global --add --bool push.autoSetupRemote true
