ce528d508de8b8ee595d72fc1dcbe32c9e650748
Julian Poyourow <julian@poyourow.com>
2025-02-02 14:28:39 -0800

    feat: disable autopairs

--- init.lua
+++ init.lua
@@ -327,38 +327,39 @@ require('lazy').setup({
   --   end
   -- },
 
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    -- Optional dependency
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function()
-      require('nvim-autopairs').setup {}
-      -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-      local npairs = require'nvim-autopairs'
-      local Rule = require'nvim-autopairs.rule'
-      local cond = require 'nvim-autopairs.conds'
-      npairs.add_rule(Rule('<', '>', {
-        -- if you use nvim-ts-autotag, you may want to exclude these filetypes from this rule
-        -- so that it doesn't conflict with nvim-ts-autotag
-        '-html',
-        '-javascriptreact',
-        '-typescriptreact',
-      }):with_pair(
-        -- regex will make it so that it will auto-pair on
-        -- `a<` but not `a <`
-        -- The `:?:?` part makes it also
-        -- work on Rust generics like `some_func::<T>()`
-        cond.before_regex('%a+:?:?$', 3)
-      ):with_move(function(opts)
-        return opts.char == '>'
-      end))
-    end,
-  },
+  -- Adds automatic pairs when typing various symbols
+  -- {
+  --   'windwp/nvim-autopairs',
+  --   event = 'InsertEnter',
+  --   -- Optional dependency
+  --   dependencies = { 'hrsh7th/nvim-cmp' },
+  --   config = function()
+  --     require('nvim-autopairs').setup {}
+  --     -- If you want to automatically add `(` after selecting a function or method
+  --     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+  --     local cmp = require 'cmp'
+  --     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  --
+  --     local npairs = require'nvim-autopairs'
+  --     local Rule = require'nvim-autopairs.rule'
+  --     local cond = require 'nvim-autopairs.conds'
+  --     npairs.add_rule(Rule('<', '>', {
+  --       -- if you use nvim-ts-autotag, you may want to exclude these filetypes from this rule
+  --       -- so that it doesn't conflict with nvim-ts-autotag
+  --       '-html',
+  --       '-javascriptreact',
+  --       '-typescriptreact',
+  --     }):with_pair(
+  --       -- regex will make it so that it will auto-pair on
+  --       -- `a<` but not `a <`
+  --       -- The `:?:?` part makes it also
+  --       -- work on Rust generics like `some_func::<T>()`
+  --       cond.before_regex('%a+:?:?$', 3)
+  --     ):with_move(function(opts)
+  --       return opts.char == '>'
+  --     end))
+  --   end,
+  -- },
 
   -- Multiple cursors with <C-n>
   {
