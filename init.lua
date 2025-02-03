0a888a2a1c5a0e7e839235a6b4bc97213d142d62
Julian Poyourow <julian@poyourow.com>
2025-02-03 12:50:31 -0800

    feat: nvim-ufo

--- init.lua
+++ init.lua
@@ -524,6 +524,62 @@ require('lazy').setup({
     event = "VeryLazy",
   },
 
+  {
+    'kevinhwang91/nvim-ufo',
+    version = "*",
+    dependencies = {
+      'kevinhwang91/promise-async'
+    },
+    config = function()
+      vim.o.foldcolumn = '0'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      
+      -- This displays a pretty "󰁂 ##" next to a given fold which is nicer than the default "..."
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+          local newVirtText = {}
+          local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+          local sufWidth = vim.fn.strdisplaywidth(suffix)
+          local targetWidth = width - sufWidth
+          local curWidth = 0
+          for _, chunk in ipairs(virtText) do
+              local chunkText = chunk[1]
+              local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+              if targetWidth > curWidth + chunkWidth then
+                  table.insert(newVirtText, chunk)
+              else
+                  chunkText = truncate(chunkText, targetWidth - curWidth)
+                  local hlGroup = chunk[2]
+                  table.insert(newVirtText, {chunkText, hlGroup})
+                  chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                  -- str width returned from truncate() may less than 2nd argument, need padding
+                  if curWidth + chunkWidth < targetWidth then
+                      suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                  end
+                  break
+              end
+              curWidth = curWidth + chunkWidth
+          end
+          table.insert(newVirtText, {suffix, 'MoreMsg'})
+          return newVirtText
+      end
+
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return {'treesitter', 'indent'}
+        end,
+        fold_virt_text_handler = handler,
+        preview = {
+          win_config = {
+            -- Link the background highlight group of the preview window to the background of the editor
+            winhighlight = 'Normal:Folded',
+          },
+        }
+      })
+    end
+  },
+
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
   -- requirements installed.
