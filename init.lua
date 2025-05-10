ced35516d7de0df07c518a54a6aff28b54c9d2c7
julianpoyourow <julian@poyourow.com>
2025-05-10 17:45:08 +0000

    fix: mason

--- init.lua
+++ init.lua
@@ -90,8 +90,8 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { 'williamboman/mason.nvim', config = true, version = "~1.0.0" },
+      { 'williamboman/mason-lspconfig.nvim', version = "~1.0.0" },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP
