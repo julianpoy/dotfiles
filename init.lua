1135f00eaed3bc72f427295c3b167fcf3e8b6051
julianpoyourow <julian@poyourow.com>
2025-05-20 01:57:38 +0000

    fix: debugger

--- init.lua
+++ init.lua
@@ -798,15 +798,32 @@ require('lazy').setup({
       local dap = require 'dap'
       local dapui = require 'dapui'
 
-      dap.adapters["node"] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-          command = "js-debug-adapter",
-          args = { "${port}" },
-        },
-      }
+      for _, adapter in pairs({ "node", "chrome" }) do
+          local pwa_adapter = "pwa-" .. adapter
+
+          -- Handle launch.json configurations
+          -- which specify type as "node" or "chrome"
+          -- Inspired by https://github.com/StevanFreeborn/nvim-config/blob/main/lua/plugins/debugging.lua#L111-L123
+
+          -- Main adapter
+          dap.adapters[pwa_adapter] = {
+              type = "server",
+              host = "localhost",
+              port = "${port}",
+              executable = {
+                  command = "js-debug-adapter",
+                  args = { "${port}" },
+              },
+              enrich_config = function(config, on_config)
+                  -- Under the hood, always use the main adapter
+                  config.type = pwa_adapter
+                  on_config(config)
+              end,
+          }
+
+          -- Dummy adapter, redirects to the main one
+          dap.adapters[adapter] = dap.adapters[pwa_adapter]
+      end
 
       require('mason-nvim-dap').setup {
         -- Makes a best effort to setup the various debuggers with
