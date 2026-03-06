56e3f9dbb1721d42991960b7b61eb4a08c0f9617
Julian Poyourow <julian@poyourow.com>
2026-03-06 10:01:02 -0800

    feat: re-add gitsigns.nvim

--- init.lua
+++ init.lua
@@ -327,20 +327,11 @@ require('lazy').setup({
     opts = {}
   },
 
-  -- Adds git releated signs to the gutter, as well as utilities for managing changes
-  -- {
-  --   'lewis6991/gitsigns.nvim',
-  --   opts = {
-  --     -- See `:help gitsigns.txt`
-  --     signs = {
-  --       add = { text = '+' },
-  --       change = { text = '~' },
-  --       delete = { text = '_' },
-  --       topdelete = { text = '‾' },
-  --       changedelete = { text = '~' },
-  --     },
-  --   },
-  -- },
+  -- Adds git related signs to the gutter to visualize diffs.
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {},
+  },
 
   -- Syntax theme
   {
